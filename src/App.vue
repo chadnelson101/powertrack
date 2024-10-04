316ebd10e887e7e02ede125fd3887b83f1da4ac9
chadnelson101 <chadnelson279@gmail.com>
2024-10-04 13:51:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <Navbar />
+  <router-view />
+  <FooterComp/>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import FooterComp from "./components/FooterComp.vue";
+export default {
+  components: { Navbar, FooterComp },
+};
+</script>
+
+<style>
+#app {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+}
+
+body {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/calculator',
+    name: 'calculator',
+    component: () => import('../views/CalculatorView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('../views/ContactView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import('../views/SignUpView.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
